70dffb0c0ce43a5b56c855c778e6ad4acf69f68f
Rafael Costa <rafael1_mc@hotmail.com>
2024-12-22 13:25:28 -0300

    example of clear all and output status

--- main.go
+++ main.go
@@ -7,10 +7,20 @@ import (
 	"epub-tts/internal/file"
 	"epub-tts/internal/tts"
 	"fmt"
+	"time"
 )
 
 func main() {
 	fmt.Println(" ---== Execution Started ==--- ")
+
+	time.Sleep(2 * time.Second)
+	foo()
+	time.Sleep(2 * time.Second)
+	bar()
+	time.Sleep(2 * time.Second)
+	foobar()
+
+	return
 
 	err := file.CreateOutputDir(consts.TmpOutputFolderName)
 	if err != nil {
@@ -30,8 +40,41 @@ func main() {
 	}
 	debug.GenerateDebugFiles(epub)
 
-	tts := tts.NewTTS(3, textBook)
+	tts := tts.NewTTS(8, textBook)
 	tts.Run()
 
 	fmt.Println(" ---== Execution ended ==--- ")
+}
+
+func foo() {
+	fmt.Printf("\033[0;0H")
+	fmt.Println("Item1: Done                                                  	")
+	fmt.Println("Item2: Converting												")
+	fmt.Println("Item3: Text-to-speech											")
+	fmt.Println("Item4: Waiting													")
+	fmt.Println("Item5: Waiting													")
+	fmt.Println("Item6: Waiting													")
+	fmt.Println("Item7: Waiting													")
+}
+
+func bar() {
+	fmt.Printf("\033[0;0H")
+	fmt.Println("Item1: Done                                                  	")
+	fmt.Println("Item2: Done													")
+	fmt.Println("Item3: Converting												")
+	fmt.Println("Item4: Text-to-speech											")
+	fmt.Println("Item5: Text-to-speech											")
+	fmt.Println("Item6: Waiting													")
+	fmt.Println("Item7: Waiting													")
+}
+
+func foobar() {
+	fmt.Printf("\033[0;0H")
+	fmt.Println("Item1: Done                                                  	")
+	fmt.Println("Item2: Done													")
+	fmt.Println("Item3: Done													")
+	fmt.Println("Item4: Done													")
+	fmt.Println("Item5: Done													")
+	fmt.Println("Item6: Waiting													")
+	fmt.Println("Item7: Waiting													")
 }
