0be4fff0affb061e0154aa102c2de8280657e21b
Rafael Costa <rafael1_mc@hotmail.com>
2025-01-01 18:09:58 -0300

    rename input dir

--- internal/consts/consts.go
+++ internal/consts/consts.go
@@ -2,7 +2,7 @@ package consts
 
 const (
 	Perm          = 0777
-	InputFilePath = "volume/input.epub"
+	InputFilePath = "input/Dracula.epub"
 
 	IsDryRun = false // if true, will generate text files, but not audio files
 	IsDebug  = false // if true, will generate files for section json and html content as well
